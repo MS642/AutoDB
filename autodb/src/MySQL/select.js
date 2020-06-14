5fec3659ebf5b176f12735310ff7538089abd52b
Mohamed <mabouzadi642@gmail.com>
2020-06-14 15:52:05 -0700

    mySQL select

---
+++ autodb/src/MySQL/select.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+//Create A connection
+var con = mysql.createConnection({
+  host: "localhost",
+  port: "3308",
+  user: "root",
+  password: "",
+  database: "autodb",
+})
+
+
+  con.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected To the database:");
+ 
+    var sqlQuery = "SELECT * FROM customer";
+    con.query(sqlQuery, function(err, result) {
+        if(err) throw err;
+        // console.log(result);
+        // console.log(result[0]);
+        // console.log(result[0].name);
+        for(var i = 0; i< result.length; i++) {
+            console.log(result[i].name);
+        }
+    });
+  })
